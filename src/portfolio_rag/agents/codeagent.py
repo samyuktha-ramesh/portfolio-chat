36e8ab320d08fe496104988c9e71b67f1a3b5fc4
alexander-brady <bradya@student.ethz.ch>
2025-09-10 22:54:44 +0200

    fix: logs now correctly surpressed

--- src/portfolio_rag/agents/codeagent.py
+++ src/portfolio_rag/agents/codeagent.py
@@ -1,14 +1,36 @@
+import hydra
 from omegaconf import DictConfig
+from rich.console import Console
 from smolagents import CodeAgent, OpenAIModel
+from smolagents.monitoring import AgentLogger
+
+logger = None
+
+
+def init_logger():
+    global logger
+    if logger is None:
+        hydra_wd = hydra.core.hydra_config.HydraConfig.get().runtime.output_dir  # type: ignore
+        logger = AgentLogger(
+            console=Console(
+                file=open(
+                    f"{hydra_wd}/codeagent.log", "a", encoding="utf-8", errors="replace"
+                ),
+                force_terminal=False,
+                width=120,
+            )
+        )
 
 
 def run_codeagent(cfg: DictConfig, system_prompt: str, query: str) -> str:
+    init_logger()
     model = OpenAIModel(model_id=cfg.model.name, api_key=cfg.model.api_key)
     agent = CodeAgent(
         tools=[],
         model=model,
         add_base_tools=True,
         additional_authorized_imports=["pandas"],
+        logger=logger,
     )
     response = agent.run(system_prompt + f" Query: {query}")
     return str(response)
